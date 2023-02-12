fbf9973bb464938e57822ada8f72373c110a6966
Charles Frye <cfrye59@gmail.com>
2023-02-11 19:09:15 -0800

    Initialize exp/change_trainer

---
+++ run.py
@@ -0,0 +1,149 @@
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+import argparse
+import zenml
+from zenml.client import Client
+from zenml.config import DockerSettings
+
+from pipelines import (
+    development_pipeline,
+    staging_train_and_deploy_pipeline,
+    prod_train_and_deploy_pipeline
+)
+
+from steps.data_loaders import (
+    development_data_loader,
+    production_data_loader,
+    staging_data_loader,
+)
+from steps.evaluators import evaluator
+from steps.trainers import TrainerParams, svc_trainer
+
+from utils.kubeflow_helper import get_kubeflow_settings
+
+
+def main(stage: str = "local"):
+    """Main runner for all three pipelines.
+
+    Args:
+        stage: One of "local", "staging", and "production".
+        Defaults to "local".
+
+    Raises:
+        AssertionError: "If experiment tracker not in stack."
+    """
+
+    # experiment_tracker = Client().active_stack.experiment_tracker
+
+    settings = {}
+    
+    # if experiment_tracker is None:
+    #     raise AssertionError("Experiment Tracker needs to exist in the stack!")
+    
+    if stage == "local":
+        # initialize and run the training pipeline
+        training_pipeline_instance = development_pipeline(
+            importer=development_data_loader(),
+            trainer=svc_trainer(
+                params=TrainerParams(
+                    degree=1,
+                )
+            ),
+            evaluator=evaluator(),
+        )
+
+    elif stage == "staging":
+        # initialize the staging pipeline with a new data loader        
+        docker_settings = DockerSettings(
+            required_integrations=["sklearn"],
+            build_options={
+                "buildargs": {
+                    "ZENML_VERSION": f"{zenml.__version__}"
+                },
+            },
+        )
+        
+        training_pipeline_instance = staging_train_and_deploy_pipeline(
+            importer=staging_data_loader(),
+            trainer=svc_trainer(
+                params=TrainerParams(
+                    degree=1,
+                )
+            ),
+            evaluator=evaluator(),
+        )
+        
+        settings = {
+            "orchestrator.kubeflow": get_kubeflow_settings(),
+            "docker": docker_settings,
+        }
+
+    elif stage == "production":
+        from steps.deployment_triggers import deployment_trigger
+        from steps.model_deployers import sklearn_model_deployer
+        
+        # docker settings for production
+        docker_settings = DockerSettings(
+            required_integrations=["sklearn", "kserve"],
+            install_stack_requirements=False,
+            requirements=[
+                "boto3==1.21.0",
+                "kfp==1.8.13",
+                "kserve==0.9.0",
+                "s3fs==2022.3.0",
+                "sagemaker==2.82.2",
+                "scikit-learn",
+                "torch-model-archiver",
+            ]
+        )
+        
+        # initialize and run the training pipeline in production
+        training_pipeline_instance = prod_train_and_deploy_pipeline(
+            importer=production_data_loader(),
+            trainer=svc_trainer(
+                params=TrainerParams(
+                    degree=1,
+                )
+            ),
+            evaluator=evaluator(),
+            deployment_trigger=deployment_trigger(),
+            model_deployer=sklearn_model_deployer,
+        )
+        settings = {
+            "orchestrator.kubeflow": get_kubeflow_settings(),
+            "docker": docker_settings,
+        }
+
+    # Run pipeline
+    training_pipeline_instance.run(
+        settings=settings
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--stage",
+        default="local",
+        help="Toggles which pipeline to run. One of `local`, "
+        "`staging`, and `production`. Defaults to `local`",
+        type=str,
+        required=False,
+    )
+    args = parser.parse_args()
+    
+    assert args.stage in ["local", "staging", "production"]
+    main(args.stage)
---
+++ steps/data_loaders.py
@@ -0,0 +1,90 @@
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from zenml.steps import Output, step
+import requests
+
+
+def download_dataframe(
+    bucket: str = "zenmlpublicdata",
+    env: str = "staging",
+    df_name: str = "X_train",
+    df_type: str = "dataframe",
+    
+) -> pd.DataFrame:
+    filename = "df.csv"
+    url = f'https://{bucket}.s3.eu-central-1.amazonaws.com/{env}/{df_name}/{filename}'
+    r = requests.get(url, allow_redirects=True)
+
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+
+    with open(filename, 'rb') as f:
+        df = pd.read_csv(f)
+
+    if df_type == "series":
+        # Taking the first column if its a series as the assumption
+        # is that there will only be one
+        df = df[df.columns[0]]
+
+    return df
+
+@step
+def development_data_loader() -> Output(
+    X_train=pd.DataFrame,
+    X_test=pd.DataFrame,
+    y_train=pd.Series,
+    y_test=pd.Series,
+):
+    """Load the local dataset."""
+    iris = load_iris(as_frame=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        iris.data, iris.target, test_size=0.2, shuffle=True, random_state=42
+    )
+    return X_train, X_test, y_train, y_test
+
+
+@step(enable_cache=False)
+def staging_data_loader() -> Output(
+    X_train=pd.DataFrame,
+    X_test=pd.DataFrame,
+    y_train=pd.Series,
+    y_test=pd.Series,
+):
+    """Load the static staging dataset."""
+    X_train = download_dataframe(env="staging", df_name="X_train")
+    X_test = download_dataframe(env="staging", df_name="X_test")
+    y_train = download_dataframe(env="staging", df_name="y_train", df_type="series")
+    y_test = download_dataframe(env="staging", df_name="y_test", df_type="series")
+
+    return X_train, X_test, y_train, y_test
+
+
+@step(enable_cache=False)
+def production_data_loader() -> Output(
+    X_train=pd.DataFrame,
+    X_test=pd.DataFrame,
+    y_train=pd.Series,
+    y_test=pd.Series,
+):
+    """Load the production production dataset."""
+    X_train = download_dataframe(env="production", df_name="X_train")
+    X_test = download_dataframe(env="production", df_name="X_test")
+    y_train = download_dataframe(env="production", df_name="y_train", df_type="series")
+    y_test = download_dataframe(env="production", df_name="y_test", df_type="series")
+
+    return X_train, X_test, y_train, y_test
